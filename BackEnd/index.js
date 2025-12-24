9e39be48e8f16330de0bebe3cfb32d1eb2dd74ea
Alexander James A Reyes <120449034+BrutRL@users.noreply.github.com>
2025-12-24 12:38:02 +0800

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -23,7 +23,7 @@ const limiter = rateLimit({
 });
 
 await DbConnection();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://expense-tracker-frontend-vc14.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(upload.array());
