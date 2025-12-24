70164f6937690b72c366de6c4afc617d1ac76b8a
Alexander James A Reyes <120449034+BrutRL@users.noreply.github.com>
2025-12-24 12:57:23 +0800

    Update AuthControlller.js

--- BackEnd/Controller/AuthControlller.js
+++ BackEnd/Controller/AuthControlller.js
@@ -68,12 +68,13 @@ export const login = async (req, res) => {
       { expiresIn: rememberMe ? "30d" : "3h" } // longer expiry if rememberMe
     );
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
-      maxAge: rememberMe ? 30 * 24 * 60 * 60 * 1000 : 3 * 60 * 60 * 1000, // 30 days vs 3 hours
-    });
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,        // required for HTTPS in production
+  sameSite: "none",    // allow cross-site cookies
+  maxAge: rememberMe ? 30 * 24 * 60 * 60 * 1000 : 3 * 60 * 60 * 1000,
+});
+
 
     return res
       .status(200)
